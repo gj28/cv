83696dd054df77e2fdf0f746b3614d067d49accd
Gaurav Jadhav <gauravjadhav173@gmail.com>
2024-03-08 15:00:39 +0530

    portf

--- src/cta.js
+++ src/cta.js
@@ -4,7 +4,7 @@ class CTA extends React.Component {
     render() {
         return(
             <div className="cta">
-                <a className="btn" href="https://drive.google.com" target="_blank">Download Resume</a>
+                <a className="btn" href="https://drive.google.com/drive/folders/1ysiF8jfDbxQVfgWIvuw0jHZcZP1l4Lmc?usp=sharing" target="_blank">Download Resume</a>
                 <a className="btn btn-primary" href="#contact">Let's Talk</a>
             </div>
         );
